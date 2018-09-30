aa7882ffc51f7e367a9c99178384469b63874724
Jonay López <jonayefren@gmail.com>
2018-09-30 22:14:21 +0100

    my first commit

---
+++ src/activity1/Activity1.java
@@ -0,0 +1,13 @@
+
+package activity1;
+
+import java.util.Arrays;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+        
+    }
+   
+    
+}
---
+++ src/activity1/MathOperations.java
@@ -0,0 +1,7 @@
+package activity1;
+
+public class MathOperations {
+
+    
+
+}
